98727186257262992ee4605ef7aef41d80226c22
GarethWoodman <gareth.woodman92@gmail.com>
2020-06-24 14:46:26 +0100

    Tested if hero moves to right

--- covid_game/src/hero.js
+++ covid_game/src/hero.js
@@ -1,5 +1,10 @@
 export default class Hero {
   constructor() {
+    this.SPEED = 1
     this.position = { x: 200, y: 200 }
+  }
+
+  moveRight() {
+    this.position.x += this.SPEED;
   }
 }
--- covid_game/src/hero.test.js
+++ covid_game/src/hero.test.js
@@ -6,4 +6,11 @@ describe('Hero', () => {
     const hero = new Hero();
     expect(hero.position).toEqual({ x: 200, y: 200});
   })
+
+  test('moves right', () => {
+    const hero = new Hero();
+    var current_x_position = hero.position.x
+    hero.moveRight()
+    expect(hero.position.x).toEqual(current_x_position + hero.SPEED)
+  })
 })
