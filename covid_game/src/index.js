98898218ec7e5dce28fafc33f1c290024e5e46d5
GarethWoodman <gareth.woodman92@gmail.com>
2020-06-24 15:22:30 +0100

    'Spiked' moveRight function on hero

--- covid_game/src/index.js
+++ covid_game/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import Header from './components/header';
+import Hero from '../src/hero'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,6 +18,21 @@ let canvas = document.getElementById("gameScreen");
 let ctx = canvas.getContext("2d");
 
 ctx.fillRect(20, 20, 100, 100);
+
+let hero = new Hero();
+hero.draw(ctx);
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+    e = e || window.event;
+    if (e.keyCode == '39') {
+      console.log(hero);
+      hero.moveRight()
+      ctx.clearRect(0, 0, 800, 600);
+      hero.draw(ctx)
+    }
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
