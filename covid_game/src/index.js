8b827b8db23a4737d4db512d53f88b33d581c98e
LTurns <lizzieturney@gmail.com>
2020-06-24 15:26:11 +0100

    adding sprite animation

--- covid_game/src/index.js
+++ covid_game/src/index.js
@@ -13,11 +13,48 @@ ReactDOM.render(
 );
 
 let canvas = document.getElementById("gameScreen");
-
 let ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 600;
 
-ctx.fillRect(20, 20, 100, 100);
+// ctx.fillRect(20, 20, 100, 100);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+var img = new Image();
+img.src = "../public/assets/sprite.png";
+
+function sprite (options) {
+
+    var that = {};
+
+    that.context = options.context;
+    that.width = options.width;
+    that.height = options.height;
+    that.image = options.image;
+
+    that.render = function () {
+
+      // Draw the animation
+      that.context.drawImage(
+         that.image,
+         0,
+         0,
+         that.width,
+         that.height,
+         0,
+         0,
+         that.width,
+         that.height);
+  };
+
+    return that;
+
+}
+
+var person = sprite({
+    context: canvas.getContext("2d"),
+    width: 100,
+    height: 100,
+    image: img
+});
+
+person.render()
