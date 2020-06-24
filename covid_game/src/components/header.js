1726a560a8885b8b2a0473a107218ccb882a8498
LTurns <lizzieturney@gmail.com>
2020-06-24 13:34:11 +0100

    Component folder added with Header for title

---
+++ covid_game/src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Header(){
+  return(
+    <div className="header">
+    <center>
+    <h1> Covid Game </h1>
+    </center>
+    </div>
+  )
+}
+
+export default Header
--- covid_game/src/index.js
+++ covid_game/src/index.js
@@ -1,15 +1,16 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import * as serviceWorker from './serviceWorker';
-//
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import Header from './components/header';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 let canvas = document.getElementById("gameScreen");
 
