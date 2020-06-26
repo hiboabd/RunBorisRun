d885d863bd75138e6ecadd1ee319158a8de1f404
LTurns <lizzieturney@gmail.com>
2020-06-26 16:52:20 +0100

    mock test of player moving right

--- src/input.test.js
+++ src/input.test.js
@@ -38,4 +38,58 @@ describe('Input', () => {
     input.checkKey(e)
     expect(input.left).toBe(true)
   })
+
+  test('check key calls hero jump function', () => {
+    var ctx = {
+      fillStyle: '',
+      clearRect: function () {
+        return 'changed'
+      },
+      fillRect: function () {
+        return 'changed'
+      }
+    }
+    const hero = new Hero();
+    const input = new Input(hero, ctx);
+
+    var e = {keyCode: 38, type: "keydown"}
+
+    input.checkKey(e)
+    expect(input.up).toBe(true)
+  })
+
+  test('checks that player can move right', () => {
+
+    const hero = {
+     moveRight() {
+       return "moved";
+     },
+   };
+
+  const spy = jest.spyOn(hero, 'moveRight');
+  // hero.moveRight();
+  var input = new Input(hero)
+  input.right = true
+  input.movePlayer()
+
+  expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+
+    test('checks that player can move right', () => {
+
+      const hero = {
+       moveRight() {
+         return "moved";
+       },
+     };
+
+    const spy = jest.spyOn(hero, 'moveRight');
+    // hero.moveRight();
+    var input = new Input(hero)
+    input.right = true
+    input.movePlayer()
+
+    expect(spy).toHaveBeenCalledTimes(1);
+    });
 })
