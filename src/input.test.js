25a24b0655386dccca0c0613b8225500cdaed6fd
LTurns <lizzieturney@gmail.com>
2020-06-26 17:08:47 +0100

    tests for jump now completed

--- src/input.test.js
+++ src/input.test.js
@@ -67,7 +67,6 @@ describe('Input', () => {
    };
 
   const spy = jest.spyOn(hero, 'moveRight');
-  // hero.moveRight();
   var input = new Input(hero)
   input.right = true
   input.movePlayer()
@@ -76,20 +75,53 @@ describe('Input', () => {
   });
 
 
-    test('checks that player can move right', () => {
+  test('checks that player can move left', () => {
 
-      const hero = {
-       moveRight() {
-         return "moved";
-       },
-     };
+    const hero = {
+     moveLeft() {
+       return "moved";
+     },
+   };
 
-    const spy = jest.spyOn(hero, 'moveRight');
-    // hero.moveRight();
-    var input = new Input(hero)
-    input.right = true
-    input.movePlayer()
+  const spy = jest.spyOn(hero, 'moveLeft');
+  var input = new Input(hero)
+  input.left = true
+  input.movePlayer()
 
-    expect(spy).toHaveBeenCalledTimes(1);
-    });
+  expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+
+  test('checks that player can jump', () => {
+
+    const hero = {
+     jumping: false,
+     jump: function(){
+       return "moved";
+     },
+   };
+
+  const spy = jest.spyOn(hero, 'jump');
+  var input = new Input(hero)
+  input.up = true
+  input.movePlayer()
+
+  expect(spy).toHaveBeenCalledTimes(1);
+  });
+
+  test('if player is not moving, hero is at frame 0', () => {
+
+    const hero = {
+     frameX: 1,
+   };
+
+   var input = new Input(hero)
+   input.up = false
+   input.right = false
+   input.left = false
+   input.movePlayer()
+   expect(hero.frameX).toEqual(0);
+
+ })
+
 })
