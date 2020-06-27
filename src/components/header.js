ad5e89e6489c635ce2c82223c85229eb02dee71f
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-27 09:15:56 +0100

    Minor changes to header component

--- src/components/header.js
+++ src/components/header.js
@@ -4,9 +4,9 @@ import React from 'react';
 function Header(){
   return(
     <div className="header">
-    <center>
-    <h1> Covid Game </h1>
-    </center>
+      <center>
+      <h1> Covid Game </h1>
+      </center>
     </div>
   )
 }
