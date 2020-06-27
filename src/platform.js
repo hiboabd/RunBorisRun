9005dada960396ed008359db852dfdf040663773
GarethWoodman <gareth.woodman92@gmail.com>
2020-06-27 17:29:29 +0100

    Added new attributes to check sides of platform

--- src/platform.js
+++ src/platform.js
@@ -8,11 +8,19 @@ export default class Platform {
   }
 
   draw(ctx){
+    this.update()
     return ctx.drawImage(this.image,
       this.position.x,
       this.position.y,
       this.width,
       this.height
     );
+  }
+
+  update(){
+    this.top    = this.position.y
+    this.bottom = this.position.y + this.height
+    this.left   = this.position.x
+    this.right  = this.position.x + this.width
   }
 }
