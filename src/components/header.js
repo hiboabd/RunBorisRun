52292abec9322cd19878ebe5b096517a73bfe971
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-28 07:34:10 +0100

    Moved all links (buttons) to header component

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,14 @@ function Header(){
     <div className="header">
       <center>
       <h1> Covid Game </h1>
+      <div class="link-group">
+        <a href="/" className="button" >Home</a>
+        <a href="/rules" className="button" >Rules</a>
+        <a href="/play" className="button" >Play</a>
+        <a href="/about" className="button" >About us!</a>
+      </div>
       </center>
+      <br/>
     </div>
   )
 }
