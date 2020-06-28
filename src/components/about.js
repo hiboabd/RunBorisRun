3d425c25f6363ebacde327b2753c0adc41290bb8
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-06-28 18:26:09 +0100

    Amended about us page

--- src/components/about.js
+++ src/components/about.js
@@ -4,7 +4,13 @@ function About(){
   return(
     <div className="about">
     <center>
-      <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+      <p> Hi, we are the creators of [NAME OF GAME HERE]. </p>
+
+      <p> We are six budding software developers who joined Makers Academy in February 2020 and built [NAME OF GAME HERE] as our final project. </p>
+
+      <p> Click on our images to see our other work. </p>
+
+      
     </center>
     </div>
   )
