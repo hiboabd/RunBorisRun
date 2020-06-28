bfde3885a5165451fcd507c51dc812766520c28a
GarethWoodman <gareth.woodman92@gmail.com>
2020-06-28 09:04:38 +0100

    Added moving & movingSpeed attributes to platform

--- src/platform.js
+++ src/platform.js
@@ -1,10 +1,12 @@
 export default class Platform {
-  constructor(image, x, y, height, width){
+  constructor(image, x, y, height, width, moving = false, movingSpeed = -0.5){
     this.height = height
     this.width = width
     this.position = {x: x, y: y}
     this.image = new Image();
     this.image.src = image
+    this.moving = moving
+    this.movingSpeed = movingSpeed
   }
 
   draw(ctx){
@@ -22,5 +24,9 @@ export default class Platform {
     this.bottom = this.position.y + this.height
     this.left   = this.position.x
     this.right  = this.position.x + this.width
+  }
+
+  move(){
+    this.position.x += this.movingSpeed
   }
 }
