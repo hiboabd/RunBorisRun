435d79524ede0cc837a5fbe5719502688bb4097c
joannacollier <collj035@gmail.com>
2020-06-29 08:59:42 +0100

    refactored spiked collision detection

--- src/detectCollision.js
+++ src/detectCollision.js
@@ -25,32 +25,15 @@ export default class DetectCollision{
   hitPasserby = (passerby, input) => {
 
     var passerbyFront = passerby.position.x + 64
+    var passerbyBack = passerby.position.x
     var heroFront = this.hero.position.x + 64
+    var heroBack = this.hero.position.x 
     var jumpDirection = input.history[input.history.length - 1]
-    console.log(jumpDirection)
-
-    if(heroFront >= passerby.position.x && this.hero.position.x <= passerbyFront && this.hero.jumping === false){
-      console.log("collision")
-      if(heroFront <= (passerby.position.x + 2) ||
-      (heroFront >= passerby.position.x  && heroFront <= passerby.position.x - 2) ){
-        if(jumpDirection === 'r'){
-          this.hero.position.x = passerby.position.x - 64
-        } else {
-          this.hero.position.x = passerby.position.x + 64
-        }
-      } else if(this.hero.position.x <= passerbyFront
-      || this.hero.position.x === passerbyFront ){
-        if(jumpDirection === 'l'){
-          this.hero.position.x = passerbyFront
-        } else {
-          this.hero.position.x = passerby.position.x - 64
-        }
-
-      } else{
-        console.log("we have failed :(")
-      }
-    }
-
+   
+    if(jumpDirection === "r" && heroFront >= passerbyBack && heroBack <= passerbyFront && this.hero.jumping === false){
+      this.hero.position.x = passerby.position.x - 64
+    } else if (jumpDirection === "l" && heroBack <= passerbyFront && heroFront >= passerbyBack && this.hero.jumping === false ){
+      this.hero.position.x = passerby.position.x + 64
+    } 
   }
-
 }
