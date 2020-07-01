90b247a200d5ee21b83d2fbda6067082b85fb898
GarethWoodman <gareth.woodman92@gmail.com>
2020-07-01 14:09:30 +0100

    Hardcoded scoreboard logic

--- src/components/scoreboard.js
+++ src/components/scoreboard.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Scoreboard from '../scoreboard'
 
 function ScoreboardDisplay() {
   return(
-    <div className="ScoreboardDisplay">
+    <div>
       <center>
+        <h1> Scoreboard </h1>
+        <h2> Name - Score </h2>
+        {for (var i = 0; i < Scoreboard.board.length; i++)}
+          <p>{Scoreboard.board[i].name} - {Scoreboard.board[i].score}</p>
+         }
+        }
 
       </center>
     </div>
--- src/scoreboard.js
+++ src/scoreboard.js
@@ -1,6 +1,6 @@
 export default class Scoreboard {
 
-  static board = []
+  static board = [{name: 'Gareth', score: 100}, {name: 'Jo', score: 200}, {name: 'Ed', score: 150}]
 
   static add(score) {
       Scoreboard.board.push(score)
