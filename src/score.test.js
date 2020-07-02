341d340107f538448c6eea885d84b4891b660955
GarethWoodman <gareth.woodman92@gmail.com>
2020-07-02 11:21:18 +0100

    Added calculateDistance function in score test

--- src/score.test.js
+++ src/score.test.js
@@ -3,6 +3,12 @@ import Score from '../src/score'
 
 describe('Score', () => {
 
+  function calculateDistance(distance){
+    Score.distance      = distance
+    Score.infectionRate = 0
+    Score.infectionRateUp()
+  }
+
   test('it increase the score based on distance travelled', () => {
     Score.scoreUp()
     Score.scoreUp()
@@ -16,37 +22,27 @@ describe('Score', () => {
   })
 
   test('if distance exceeds 10 meters, infection rate increases to 0.05', () => {
-    Score.distance = 10
-    Score.infectionRate = 0
-    Score.infectionRateUp()
+    calculateDistance(10)
     expect(Score.infectionRate).toEqual(0.05)
   })
 
   test('if distance exceeds 20 meters, infection rate increases to 0.08', () => {
-    Score.distance = 20
-    Score.infectionRate = 0
-    Score.infectionRateUp()
+    calculateDistance(20)
     expect(Score.infectionRate).toEqual(0.08)
   })
 
   test('if distance exceeds 30 meters, infection rate increases to 0.11', () => {
-    Score.distance = 30
-    Score.infectionRate = 0
-    Score.infectionRateUp()
+    calculateDistance(30)
     expect(Score.infectionRate).toEqual(0.11)
   })
 
   test('if distance exceeds 40 meters, infection rate increases to 0.13', () => {
-    Score.distance = 40
-    Score.infectionRate = 0
-    Score.infectionRateUp()
+    calculateDistance(40)
     expect(Score.infectionRate).toEqual(0.13)
   })
 
   test('if distance exceeds 50 meters, infection rate remains at 0.13', () => {
-    Score.distance = 60
-    Score.infectionRate = 0
-    Score.infectionRateUp()
+    calculateDistance(60)
     expect(Score.infectionRate).toEqual(0.13)
   })
 
