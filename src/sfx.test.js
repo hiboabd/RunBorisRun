0a932af863bf6e6468a37c41e217fd16657c32ba
GarethWoodman <gareth.woodman92@gmail.com>
2020-07-02 10:31:43 +0100

    Added beforeEach in sfx test

--- src/sfx.test.js
+++ src/sfx.test.js
@@ -7,44 +7,36 @@ import music from "../src/media/bgm.mp3"
 
 describe('Sfx', () => {
 
-  test('it plays jumpMP3 when hero is jumping', () => {
+  let hero;
+  let input;
+  let audio;
+
+  beforeEach(async () => {
     window.HTMLMediaElement.prototype.play = () => { /* do nothing */ };
     window.HTMLMediaElement.prototype.pause = () => { /* do nothing */ };
-    const hero = new Hero();
-    const input = new Input()
-    const audio = new Sfx(hero, input);
+    hero = new Hero();
+    input = new Input()
+    audio = new Sfx(hero, input);
+  });
+
+  test('it plays jumpMP3 when hero is jumping', () => {
     hero.jumpSpeed = -10
     audio.gameSFX();
     expect(audio.jumpPlay).toBe(true);
   })
 
   test('it plays bumpMP3 when hero bumps into left boundary', () => {
-    window.HTMLMediaElement.prototype.play = () => { /* do nothing */ };
-    window.HTMLMediaElement.prototype.pause = () => { /* do nothing */ };
-    const hero = new Hero();
-    const input = new Input()
-    const audio = new Sfx(hero, input);
     hero.position.x = -10
     audio.gameSFX();
     expect(audio.bumpPlay).toBe(true);
   })
 
   test('it plays background music', () => {
-    window.HTMLMediaElement.prototype.play = () => { /* do nothing */ };
-    window.HTMLMediaElement.prototype.pause = () => { /* do nothing */ };
-    const hero = new Hero();
-    const input = new Input()
-    const audio = new Sfx(hero, input);;
     audio.gameMusic();
     expect(audio.musicPlay).toBe(true)
   })
 
   test('when muted it doesnt play jumpMP3 when hero is jumping', () => {
-    window.HTMLMediaElement.prototype.play = () => { /* do nothing */ };
-    window.HTMLMediaElement.prototype.pause = () => { /* do nothing */ };
-    const hero = new Hero();
-    const input = new Input()
-    const audio = new Sfx(hero, input);
     input.muted = true
     hero.jumpSpeed = -10
     audio.gameSFX();
@@ -52,11 +44,6 @@ describe('Sfx', () => {
   })
 
   test('when muted it doesnt play bumpMP3 when hero bumps into left boundary', () => {
-    window.HTMLMediaElement.prototype.play = () => { /* do nothing */ };
-    window.HTMLMediaElement.prototype.pause = () => { /* do nothing */ };
-    const hero = new Hero();
-    const input = new Input()
-    const audio = new Sfx(hero, input);
     input.muted = true
     hero.position.x = -10
     audio.gameSFX();
@@ -64,11 +51,6 @@ describe('Sfx', () => {
   })
 
   test('when muted it doesnt play background music', () => {
-    window.HTMLMediaElement.prototype.play = () => { /* do nothing */ };
-    window.HTMLMediaElement.prototype.pause = () => { /* do nothing */ };
-    const hero = new Hero();
-    const input = new Input()
-    const audio = new Sfx(hero, input);;
     input.muted = true
     audio.gameMusic();
     expect(audio.musicPlay).toBe(false)
