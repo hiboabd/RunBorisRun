7b981c23c175e4ac0089f833b9ed7a8c053f30c0
GarethWoodman <gareth.woodman92@gmail.com>
2020-07-02 11:48:09 +0100

    Cleaned up passerby test

--- src/passerby.test.js
+++ src/passerby.test.js
@@ -1,19 +1,19 @@
 import Passerby from '../src/passerby'
 
 describe('Passerby', () => {
-  test('current position', () => {
-    const passerby = new Passerby(1400);
 
+  let passerby;
+
+  beforeEach(async () => {
+    passerby = new Passerby(1400)
+  });
+
+  test('current position', () => {
     expect(passerby.position.x).toEqual(1400);
   })
 
   test('draw function calls drawImage function of ctx object', () => {
-    const passerby = new Passerby();
-    var ctx = {
-      drawImage: function () {
-        return 'changed'
-      }
-    };
+    const ctx = { drawImage: function () { return 'changed' } };
     const spy = jest.spyOn(ctx, 'drawImage');
     passerby.draw(ctx)
     expect(spy).toHaveBeenCalledTimes(1);
