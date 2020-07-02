55e7f2da65f0c09e54f42707dbffe7a8e9866dc1
LTurns <lizzieturney@gmail.com>
2020-07-02 08:24:29 +0100

    tests for score at 100% coverage

--- src/score.test.js
+++ src/score.test.js
@@ -15,4 +15,39 @@ describe('Score', () => {
     expect(Score.infectionRate).toEqual(0.04)
   })
 
+  test('if distance exceeds 10 meters, infection rate increases to 0.05', () => {
+    Score.distance = 10
+    Score.infectionRate = 0
+    Score.infectionRateUp()
+    expect(Score.infectionRate).toEqual(0.05)
+  })
+
+  test('if distance exceeds 20 meters, infection rate increases to 0.08', () => {
+    Score.distance = 20
+    Score.infectionRate = 0
+    Score.infectionRateUp()
+    expect(Score.infectionRate).toEqual(0.08)
+  })
+
+  test('if distance exceeds 30 meters, infection rate increases to 0.11', () => {
+    Score.distance = 30
+    Score.infectionRate = 0
+    Score.infectionRateUp()
+    expect(Score.infectionRate).toEqual(0.11)
+  })
+
+  test('if distance exceeds 40 meters, infection rate increases to 0.13', () => {
+    Score.distance = 40
+    Score.infectionRate = 0
+    Score.infectionRateUp()
+    expect(Score.infectionRate).toEqual(0.13)
+  })
+
+  test('if distance exceeds 50 meters, infection rate remains at 0.13', () => {
+    Score.distance = 60
+    Score.infectionRate = 0
+    Score.infectionRateUp()
+    expect(Score.infectionRate).toEqual(0.13)
+  })
+
 })
