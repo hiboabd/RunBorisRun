309707884f30c725484589a6aaf1d973ad0aa7b1
joannacollier <collj035@gmail.com>
2020-07-23 14:47:25 +0100

    API fetch call to backend

--- src/components/scoreboard.js
+++ src/components/scoreboard.js
@@ -1,46 +1,42 @@
 import React from 'react';
-import Scoreboard from '../scoreboard'
-
 
 class ScoreboardDisplay extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      scoreboard: ""
+    }
+  };
 
-  // iteration = () => { 
-    
-  //   var increment = (Object.keys(Scoreboard.board).length)
-
-  //   for (var i = 0; i < Scoreboard.board.length; i++) {
-  //     var name = Scoreboard.board[i].name 
-  //     var score = Scoreboard.board[i].score
-
-  //     increment ++ 
-      
-  //     return name + " - " + score
-  //   }
-  
-
-  render() {
-    return (
-       <div>
-         
-          <center>
-            {Scoreboard.board.map((item)=><p>{item.name} - {item.score}</p>)}
-          </center>
-               
-       </div>
-    );
+  componentDidMount(){
+    this.fetchData('http://localhost:3000/')
   }
+
+  fetchData = (apiToFetch) => {
+    fetch(apiToFetch)
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+        this.setState({
+          scoreboard: data,
+        })
+      })
+  }
+
+
+
+  render(){
+    return(
+    <div id="scoreboard">
+      <center><h1>ScoreBoard</h1>
+  
+      </center>
+      
+    </div>
+    )
+  }
+
+
 }
-
-//   render() {
-//     return (
-//       <center>
-//         <div class="commentBox">
-//           <p> {this.iteration()} </p>
-//         </div>
-//       </center>
-
-//     )
-//   }
-// }
 
 export default ScoreboardDisplay
